659594d80c47506e81e0e1112abe85da71c85a20
XZA <1014115674@qq.com>
2020-12-14 12:43:07 +0800

    Update Teacher evaluation.js

--- Teacher evaluation.js
+++ Teacher evaluation.js
@@ -1,6 +1,6 @@
 var str1 = "课堂气氛非常好                           " //这里自行修改为你最喜欢什么
 var str2 = "老师讲的非常好                           " //这里自行修改为对老师的建议
-for(var i =0; i<292; i++) { 
+for(var i =0; i<360; i++) { 
     if($("input[name='item_"+i+"']").length) 
         $("input[name='item_"+i+"']").get(0).checked=true; 
 }
